d11b0a2a661934906ec88180d5b24f03c24ed02c
Mahmut KÜÇÜK <mkucuk@vphq.veripark.com>
2016-12-06 09:27:57 +0300

    zaaaaaaaaaaaaaaa

---
+++ Training1/Training1/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training1
+{
+    class Dog : Animal
+    {
+        private string breed;
+
+        public string Breed
+        {
+            get
+            {
+                return breed;
+            }
+            set
+            {
+                breed = value;
+            }
+        }
+    }
+}
---
+++ Training1/Training1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Spot = new Dog();
+            Spot.Color = "Brown";
+        }
+    }
+}
